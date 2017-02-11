2f502f58a52fe0a2462379e948ab1a0eddfdcff5
Francisco Paliouras <franciscopaliouras@franciscos-mbp.wireless.rit.edu>
2017-02-11 12:55:14 -0500

    handeled the value for the serch bar

--- Bobcat 1.0/js/main.js
+++ Bobcat 1.0/js/main.js
@@ -2,6 +2,16 @@
  * Created by franciscopaliouras on 2/11/17.
  */
 
+//submit handdled
+$('#search_form').submit(function () {
+    return false;
+});
 
 
+$("#search_btn").click(function () {
 
+    var strAdd = "<h1>";
+    strAdd += $("#search_bars").val();
+    strAdd += "</h1>"
+    $("#graph_body").append(strAdd);
+});
