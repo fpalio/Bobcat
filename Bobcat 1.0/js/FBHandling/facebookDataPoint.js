8113f7f42a31de4a22b03f89905982da22ad7aea
Francisco Paliouras <franciscopaliouras@franciscos-mbp.wireless.rit.edu>
2017-02-11 21:54:16 -0500

    Pulled more data

--- Bobcat 1.0/js/FBHandling/facebookDataPoint.js
+++ Bobcat 1.0/js/FBHandling/facebookDataPoint.js
@@ -1,42 +1,83 @@
-var idToReaction = {};
 var idToPost = {};
+var monthYearToUsers = {};
 
-function Post(id, time, reactions)
+function Post(id, time, reactions, comments)
 {
     this.id = id;
     this.time = time;
-    this.reactions = reactions.length;
+    this.reactions = reactions;
+    this.comments = comments;
 }
-
-Post.prototype
-{
-    constructor:Post;
-}
-
-/*
-console.log("Love: " + dict["LOVE"]);
-console.log("HAHA: " + dict["HAHA"]);
-console.log("Wow: " + dict["WOW"]);
-console.log("Sad: " + dict["SAD"]);
-console.log("Angry: " + dict["ANGRY"]);
-*/
 
 function Reactions()
 {
     this.reactions = {};
-    dict["LOVE"] = 0;
-    dict["HAHA"] = 0;
-    dict["WOW"] = 0;
-    dict["SAD"] = 0;
-    dict["ANGRY"] = 0;
+    this.reactions["LOVE"] = 0;
+    this.reactions["HAHA"] = 0;
+    this.reactions["WOW"] = 0;
+    this.reactions["SAD"] = 0;
+    this.reactions["ANGRY"] = 0;
+
+    this.users = new Set();
 
     this.interactions = 0;
 
 }
 
-Reactions.prototype
-{
-    constructor:Reactions;
+function Comments()
+{   this.users = {};
+    this.commentCount = 0;
+}
+
+function getItemKey(item){
+    return item.created_time.substr(0,7);
+}
+
+function processReactions(reaction, data) {
+    // /check all reactions in the post
+    for (var reaction_id in data.reactions.data) {
+        if (data.reactions.data[reaction_id] != undefined) {
+            if (data.reactions.data[reaction_id].type != "LIKE") {
+                reaction.reactions[data.reactions.data[reaction_id].type] += 1;
+                reaction.users.add(data.reactions.data[reaction_id].id);
+            }
+
+            addUsers(getItemKey(data), data.reactions.data[reaction_id].id, "reaction");
+            reaction.interactions += 1;
+        }
+    }
+}
+
+function addUsers(key, id){
+    //if the key doesn't exist create a new dictionary
+    if(!monthYearToUsers[key]){
+        monthYearToUsers[key] = new Set();
+    }
+
+
+    //add the user
+    monthYearToUsers[key].add(id);
+}
+
+function processComments(comments, data) {
+
+    //if there are comments available
+    if(data.comments != undefined) {
+
+        //response.data[post].comments.data[0].created_time
+        //for each comment_id in the comments data
+
+        for (var comment_id in data.comments.data) {
+
+                //get the key (year-month) from data[id of comment]
+                var key = getItemKey(data.comments.data[comment_id]);
+
+
+            //add the the id to the key
+                addUsers(key, data.comments.data[comment_id].from.id, "comment");
+        }
+    }
+
 }
 
 function processResponse(response)
@@ -46,43 +87,33 @@ function processResponse(response)
     var interactions = 0;
 
     //for each post in the response
-    for(var post in response.posts.data)
+    for(var post in response.data)
     {
-
         var reaction = new Reactions();
+        var comment = new Comments();
 
         //if there are any reactions
-        if(response.posts.data[post].reactions != undefined) {
-            //check all reactions in the post
-            for (var reaction in response.posts.data[post].reactions.data) {
-                if (response.posts.data[post].reactions.data[reaction] != undefined) {
-                    if(response.posts.data[post].reactions.data[reaction].type != "LIKE") {
-
-                        reaction.reactions[response.posts.data[post].reactions.data[reaction].type] += 1;
-                    }
-
-                    reaction.interactions += 1;
-                }
+        if(response.data[post].reactions != undefined) {
+                processReactions(reaction, response.data[post]);
             }
+
+        processComments(comment, response.data[post]);
+        var post_object = new Post(response.data[post].id, response.data[post].created_time, reaction);
+
+        if(!idToPost[response.data[post].id])
+        {
+            idToPost[response.data[post].id] = new Set();
         }
 
-        var post = new Post(response.posts.data[post].id, response.posts.data[post].time, reaction);
-        idToPost[response.posts.data[post].id] = post;
+        idToPost[response.data[post].id].add(post_object);
 
-    }
-
-
-    for( post in idToPost)
-    {
-        print(post);
-        print(idToPost[post].reactions);
     }
     
 }
 
-function pullReactionData(url)
+function pullData(url)
 {
-    var next = url + "/posts?fields=reactions.limit(100),created_time";
+    var next = url + "/posts?fields=reactions,created_time,comments{created_time,from}&limit=100";
     FB.api(
         next,
         function (response) {
@@ -93,5 +124,3 @@ function pullReactionData(url)
         }
     );
 }
-
-
--- Bobcat 1.0/js/main.js
+++ Bobcat 1.0/js/main.js
@@ -26,6 +26,20 @@ function login() {
     FB.getLoginStatus(function(response) {
         if (response.status === 'connected') {
             console.log('Logged in.');
+
+            // this function runs when the DOM is ready, i.e. when the document has been parsed
+            pullData("velogig");
+            calculateData();
+
+            console.log(dataChart);
+
+            makeData('line', 'line', [dataChart], ['cats', 'dog']);
+
+            makeData('bar', 'bar', [[12, 19, 3, 17, 6, 3, 7, 8, 10, 9, 10, 19], [2, 29, 5, 5, 2, 3, 10]], ['car', 'dog']);
+
+            makeData('pie', 'pie', [[12, 19, 3, 17, 6, 3, 7, 8, 10, 9, 10, 19], [2, 29, 5, 5, 2, 3, 10]], ['car', 'dog']);
+
+            document.getElementById("user-greeting").textContent = "Welcome back, Bart";
         }
         else {
             FB.login();
